adcebb91d6dfcba1ab9a464224f7996820c1daec
Ahmed Alsharif (nullp0tr) <ahmeds2000x@gmail.com>
2017-12-14 18:29:00 +0100

    Added dplot

--- MyoGrapher/__init__.py
+++ MyoGrapher/__init__.py
@@ -5,7 +5,38 @@ class MyoGrapher(object):
     def __init__(self, width=1200, height=400):
         self.width, self.height = width, height
         self.screen = pygame.display.set_mode((self.width, self.height))
-        self.last_values = None
+        self.last_values = []
+        self.dlast_values = []
+
+    def dplot(self, vals, shifts, colors):
+        division_lines = 4
+        drift = 5
+        self.screen.scroll(-drift)
+        self.screen.fill((0, 0, 0),
+                         (self.width - drift, 0, self.width, self.height))
+
+        for n, values in enumerate(vals):
+            values = [val / float(shifts[n]) for val in values]
+            if len(self.dlast_values) < len(vals):
+                self.dlast_values.append(values)
+                return
+
+            for i, (u, v) in enumerate(zip(self.dlast_values[n], values)):
+                pygame.draw.line(self.screen, colors[n],
+                                (self.width - drift, int(
+                                    self.height / division_lines * (
+                                        i + 1 - u))),
+                                (self.width, int(
+                                    self.height / division_lines * (
+                                        i + 1 - v))))
+                pygame.draw.line(self.screen, (255, 255, 255),
+                                 (self.width - drift, int(
+                                     self.height / division_lines * (
+                                         i + 1))),
+                                 (self.width, int(
+                                     self.height / division_lines * (
+                                         i + 1))))
+            self.dlast_values[n] = values
 
     def plot(self, values, drawlines=False, curve=True):
         if self.last_values is None:
@@ -36,5 +67,5 @@ class MyoGrapher(object):
 
         pygame.display.flip()
 
-    def emg_plot(self, emg, shift=2000, drawlines=False, curve=True):
+    def emg_plot(self, emg, shift=512, drawlines=False, curve=True):
         self.plot([e / float(shift) for e in emg], drawlines=drawlines, curve=curve)
